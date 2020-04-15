04fc290e2323e5f2c03f317519252e798a7cc323
PeterYama <35582343+PeterYama@users.noreply.github.com>
2020-04-15 20:44:43 +1000

    minnor changes in the job registration form

--- admin/src/pages/jobRegistration.js
+++ admin/src/pages/jobRegistration.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
+import { Form, Container, Button } from 'react-bootstrap'
 
 export default class jobRegistration extends Component {
     render() {
         return (
-            <div>
-                <p>This is the Job Registration Page</p>
-            </div>
+            <Container className="jobRegistrationProfile">
+                <form>
+                    <Form.Group controlId="">
+                        <Form.Label>I am looking for</Form.Label>
+                        <Form.Control placeholder="example: Web Developer / JavaScript Developer" />
+                    </Form.Group>
+                    <br></br>
+                    <Form.Group controlId="exampleForm.ControlSelect1">
+                        <Form.Label>Type of Developer</Form.Label>
+                        <Form.Control as="select">
+                            <option>Frontend</option>
+                            <option>Backend</option>
+                            <option>Full-Stack</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <br></br>
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control as="textarea" rows="3" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </form>
+            </Container>
         )
     }
 }
